6ef4a4cc793431890c260629d9125e7e5db72884
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-10-18 18:33:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,64 +7,32 @@ class TestDictionary(unittest.TestCase):
         result = subprocess.run([path], input=input_data, text=True, capture_output=True)
         return result.stdout, result.stderr
 
-    def test_next_word(self):
-        input_data = "next word"
-        expected_out = "next"
+    def test_1(self):
+        input_data = "test1"
+        expected_out = "test1_1"
         expected_err = ""
         output, error = self.run_program(input_data)
         self.assertEqual(output.strip(), expected_out)
         self.assertEqual(error.strip(), expected_err)
 
-    def test_vsem_privet(self):
-        input_data = "vsem privet, ya tut"
-        expected_out = "i tebe privet"
+    def test_2(self):
+        input_data = "test2"
+        expected_out = "test2_2"
         expected_err = ""
         output, error = self.run_program(input_data)
         self.assertEqual(output.strip(), expected_out)
         self.assertEqual(error.strip(), expected_err)
 
-    def test_empty(self):
-        input_data = ""
-        expected_out = "pusto"
-        expected_err = ""
-        output, error = self.run_program(input_data)
-        self.assertEqual(output.strip(), expected_out)
-        self.assertEqual(error.strip(), expected_err)
-
-    def test_space(self):
-        input_data = " "
-        expected_out = "nemnogo pusto"
-        expected_err = ""
-        output, error = self.run_program(input_data)
-        self.assertEqual(output.strip(), expected_out)
-        self.assertEqual(error.strip(), expected_err)
-
-    def test_buffer_overflow(self):
-        input_data = "eto perepolnenie " * 15
+    def test_buffer_exception(self):
+        input_data = "test1" * 20
         expected_out = ""
         expected_err = "Buffer overflow"
         output, error = self.run_program(input_data)
         self.assertEqual(output.strip(), expected_out)
         self.assertEqual(error.strip(), expected_err)
 
-    def test_oleg(self):
-        input_data = "oleg"
-        expected_out = "Privet!"
-        expected_err = ""
-        output, error = self.run_program(input_data)
-        self.assertEqual(output.strip(), expected_out)
-        self.assertEqual(error.strip(), expected_err)
-
-    def test_first_item(self):
-        input_data = "first item"
-        expected_out = "the first one"
-        expected_err = ""
-        output, error = self.run_program(input_data)
-        self.assertEqual(output.strip(), expected_out)
-        self.assertEqual(error.strip(), expected_err)
-
-    def test_ne_oleg(self):
-        input_data = "ne oleg"
+    def test_exception(self):
+        input_data = "test3"
         expected_out = ""
         expected_err = "Word not found"
         output, error = self.run_program(input_data)
