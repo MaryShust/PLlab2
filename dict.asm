fa1627106e747d3461acfbaebe93a206b89bcd3b
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-10-19 19:05:28 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -1,36 +1,27 @@
 %include "dict.inc"
 
 find_word:
-    push rbx
-    push r12
-    push r13
-    sub rsp, 8
-    mov r12, rsi        ; Устанавливаем указатель на первый элемент словаря
-    xor rax, rax        ; Обнуляем rax (используется для хранения результата)
+	push r12
+	push r13
+	xor rax, rax
+	mov r12, rdi
 .loop:
-    test r12, r12       ; Проверяем, не нулевой ли указатель
-    jz .not_found       ; Если да, то переходим к .not_found
-    lea rsi, [r12 + 8]  ; Указываем на ключ текущего элемента словаря
-    push rdi
-    push rsi
-    call string_length   ; Получаем длину ключа
-    mov r13, rax        ; Сохраняем длину в r13
-    pop rsi
-    pop rdi
-    call string_equals   ; Сравниваем строки
-    test rax, rax        ; Проверяем, равны ли строки
-    jz .next             ; Если не равны, переходим к следующему элементу
-    ; Слова совпали
-    lea rax, [r12 + r13 + 8] ; Указываем на адрес начала совпадения
-    jmp .end             ; Переходим к завершению
-.next:
-    mov r12, [r12]      ; Переходим к следующему элементу словаря
-    jmp .loop            ; Продолжаем цикл
-.not_found:
-    xor rax, rax        ; Устанавливаем rax в 0 (что означает, что не нашли)
-.end:
-    pop r13
-    pop r12
-    pop rbx
-    add rsp, 8          ; Восстанавливаем стек
-    ret
+	mov r13, rsi
+	test r13, r13
+	jz .didntfound
+	add rsi, 16
+	mov rdi, r12
+	call string_equals
+	mov rsi, r13
+	test rax, rax
+	jne .success
+	mov rsi, [rsi]
+	jmp .loop
+.success:
+	mov rax, rsi
+	pop r13
+	pop r12
+	ret
+.didntfound:
+	mov rsi, 0
+	jmp .success
