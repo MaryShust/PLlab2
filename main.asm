cb6d2410fd43bad667e595c8fb2eed03280010ab
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-10-20 12:26:41 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,65 +1,57 @@
-global _start
-section .data
-msg_noword: db "No such word",0
+%include "main.inc"
+%include "words.inc"
 
 section .data
-%include "colon.inc"
+    buffer: times 256 db 0
+    not_found_word: db "Word not found"
+    very_long: db "Buffer overflow"
 
 section .text
-%define d_err 2
-%define d_out 1
-
-extern find_word
-extern read_word
-extern string_length
-extern print_string
-extern print_newline
+global _start
 
 _start:
-	mov rsi, lw
-.link_loop:
-	mov r12, rsi  ; текущий элемент
-	mov rsi, [rsi] ; адрес предыдущего
-	test rsi, rsi
-	jz .ent
-	mov [rsi + 8], r12
-	jmp .link_loop
-.ent:
-	mov rsi, 255
-	sub rsp, 256
-	mov rdi, rsp
-	call read_word
-	test rax, rax
-	jz .bad
-	mov rdi, rax
-	mov rsi, lw
-	call find_word
-	test rax, rax
-	jz .bad
-	add rax, 16
-	mov r10, rax
-	call string_length
-	add r10, rax
-	mov rdi, r10
-	inc rdi
-	mov r15, d_out
-	call print_string
+    ; Чтение строки
+    mov rdi, buffer
+    mov rsi, 255
+    call read_str
+
+    ; Поиск слова
+    mov rdi, buffer
+    mov rsi, end_point 
+      
+    test rax, rax
+    je .overflow
+
+    call find_word
+
+    ; Проверка результат поиска
+    cmp rax, 0
+    je .not_found
+
+    ; Вывод слова
+    add rax, 8 
+    mov rdi, rax
+    call print_string
+    call print_newline
+    jmp .exit
+
+.not_found:
+    ; Сообщение о не найденном слове
+    mov rsi, not_found_word
+    mov rdx, 14
+    jmp .ng
+
+.overflow:
+    ; Сообщение о переполнении буфера
+    mov rsi, very_long
+    mov rdx, 15
+    jmp .ng
+
+.ng:
+    mov rax, 1
+    mov rdi, 2
+    syscall
+    call print_newline
+
 .exit:
-	call print_newline
-	mov rdi, rax
-	mov rax, 60	; number 'exit'
-	syscall
-.bad:
-	mov rdi, msg_noword
-	call string_length
-	mov rsi, rax
-	mov r15, d_err
-	call print_string
-	jmp .exit
-
-.list_linker:
-	; rsi - last word
-
-
-
-
+    call exit
