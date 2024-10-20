26743a28bc89f49cbe5ae218858134eb4830f5d8
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-10-20 11:52:58 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -1,39 +1,31 @@
 %include "dict.inc"
 
+section .text
+; rdi = address of a null terminated word name
+; rsi = address of the last word
+; returns: rax = 0 if not found, otherwise address
 find_word:
-    push rbx
-    push r12
-    push r13
-    sub rsp, 8
-    mov r12, rsi       
-    xor r13, r13
-
+	push r12
+	push r13
+	xor rax, rax
+	mov r12, rdi
 .loop:
-    test r12, r12      ; Проверяем, не равен ли нулю указатель
-    jz .not_found       ; Если ноль, переходим к .not_found
-
-    lea rsi, [r12 + 8]  ; Указываем rsi на текущий ключ
-    call string_length  ; Длина строки
-    mov r13, rax        ; Сохраняем длину
-    call string_equals  ; Сравниваем строки
-    cmp rax, 1          ; Проверка на равенство
-    je .found           ; Если равны, переходим к .found
-
-    mov r12, qword[r12] ; Переходим к следующему элементу
-    jmp .loop           ; Повторяем цикл
-
-.found:
-    mov rax, r12       ; Загружаем адрес найденного слова
-    add rax, r13       
-    inc rax            ; Сдвигаем указатель
-    jmp .end           ; Переходим к завершению
-
-.not_found:
-    xor rax, rax       ; Устанавливаем 0, если не найдено
-
-.end:
-    pop r13
-    pop r12             
-    pop rbx             
-    add rsp, 8         
-    ret                 
+	mov r13, rsi
+	test r13, r13
+	jz .didntfound
+	add rsi, 16
+	mov rdi, r12
+	call string_equals
+	mov rsi, r13
+	test rax, rax
+	jne .success
+	mov rsi, [rsi]
+	jmp .loop
+.success:
+	mov rax, rsi
+	pop r13
+	pop r12
+	ret
+.didntfound:
+	mov rsi, 0
+	jmp .success
