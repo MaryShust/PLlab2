2ed16b524604422ddbd656fb442de61f8ee4708f
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-10-20 12:17:44 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,60 +1,65 @@
+global _start
 section .data
-	msg: db "No word", 0
+msg_noword: db "No such word",0
 
-global _start	
-	
-section .text
+section .data
 %include "colon.inc"
 
-extern read_word
+section .text
+%define d_err 2
+%define d_out 1
+
 extern find_word
+extern read_word
 extern string_length
 extern print_string
 extern print_newline
 
-_start: 
-	
-	mov rsi, 255			; максимум, что мы можем записать в буфер
-	sub rsp, 256			; место под буфер
-	
-	mov rdi, rsp			; скажем ему куда записываем слово
-	call read_word			; оно нам вернет где начало слово (в rax)
-	
-	cmp rax, 0				; тут надо проверить если в rax 0
-	je .false
-	 
+_start:
+	mov rsi, lw
+.link_loop:
+	mov r12, rsi  ; текущий элемент
+	mov rsi, [rsi] ; адрес предыдущего
+	test rsi, rsi
+	jz .ent
+	mov [rsi + 8], r12
+	jmp .link_loop
+.ent:
+	mov rsi, 255
+	sub rsp, 256
+	mov rdi, rsp
+	call read_word
+	test rax, rax
+	jz .bad
 	mov rdi, rax
 	mov rsi, lw
-	call find_word			; возвращает указатель на строку
-	cmp rax, 0
-	je .false
-	add rax, 8				; указатель на начало строки, а не на адрес следующего слова	
-	mov r10, rax 
-	mov rdi, rax
-							; дальше нам надо найти строку по метке 
-							;(для этого надо найти сначала метку, которая лежит после строки)
-		 						
+	call find_word
+	test rax, rax
+	jz .bad
+	add rax, 16
+	mov r10, rax
 	call string_length
-	add r10, rax			; сдвинем указатель на начало метки
-	inc r10					; учитываем \0
-	mov rdi, r10	
-	
-	mov r15, 1				; дескриптор для stdout
+	add r10, rax
+	mov rdi, r10
+	inc rdi
+	mov r15, d_out
 	call print_string
-	
-.exit:	
+.exit:
 	call print_newline
-	mov rax, 60 
-	mov rdi, rax 			; чтобы echo выводило то, что у нас возвращает find_word
+	mov rdi, rax
+	mov rax, 60	; number 'exit'
 	syscall
-	ret
-	
-.false:
-	mov rdi, msg			; если нет слова - то так и напишем :)
+.bad:
+	mov rdi, msg_noword
 	call string_length
 	mov rsi, rax
-	
-	mov r15, 2				; дескриптор для ошибок
+	mov r15, d_err
 	call print_string
 	jmp .exit
-	
+
+.list_linker:
+	; rsi - last word
+
+
+
+
