58a0e9557b6d770cf68d18332b2166c7d9b1703a
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-10-20 12:17:21 +0300

    Update dict.asm

--- dict.asm
+++ dict.asm
@@ -1,51 +1,32 @@
-;section .data
-;	message:
-;		db "hello", 0
-
 global find_word
 extern string_equals
 
-%include "colon.inc"
-
-global _start
-
 section .text
-
+; rdi = address of a null terminated word name
+; rsi = address of the last word
+; returns: rax = 0 if not found, otherwise address
 find_word:
-									; rdi = address of a null terminated word name
-									; rsi = address of the last word
-									; returns: rax = 0 if not found, otherwise address
-												
-
-mov r8, rdi													
-
+	push r12
+	push r13
+	xor rax, rax
+	mov r12, rdi
 .loop:
-
-	mov r9, rsi
-	cmp rsi, 0 						; установит EF = 0, только если  rsi == null
-	je .exit_false 					; выйдем и скажем, что ничего не нашли
-	mov rdi, r8						; вернем строку в rdi
-	add rsi, 8						; смотрим что там за строка
-	call string_equals				; сравниваем строки
-	mov rsi, r9						; возвращаем rsi на место (после string_equals у нас укзатель на конец строки)
-	cmp rax, 0 						; если строки равны сохраняем указатель на нее и выходим
-	jnz .exit_true
-	mov rsi, [rsi]					; если не равны смотрим следующее слово
-    jmp .loop
-	
-.exit_true:							; если нашли строку - сохраняем указатель на нее
+	mov r13, rsi
+	test r13, r13
+	jz .didntfound
+	add rsi, 16
+	mov rdi, r12
+	call string_equals
+	mov rsi, r13
+	test rax, rax
+	jne .success
+	mov rsi, [rsi]
+	jmp .loop
+.success:
 	mov rax, rsi
-	ret 
-	
-.exit_false:
-	mov rax, 0						; если не нашли - сохраняем null
+	pop r13
+	pop r12
 	ret
-	
-	
-;_start:
-;	mov rdi, message
-;	mov rsi, lw
-;	call find_word
-;	mov rdi, rax
-;	mov rax, 60
-;	syscall	
+.didntfound:
+	mov rsi, 0
+	jmp .success
